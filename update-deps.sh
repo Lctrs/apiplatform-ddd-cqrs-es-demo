59fb57edff8c4b0bb980af693b63da69f3fd2bcc
Lctrs <jerome@prmntr.me>
2019-04-12 13:05:23 +0200

    Bump deps

--- update-deps.sh
+++ update-deps.sh
@@ -4,5 +4,5 @@ docker-compose pull
 docker-compose build --pull
 docker-compose run admin /bin/sh -c 'yarn install && yarn upgrade'
 docker-compose run client /bin/sh -c 'yarn install && yarn upgrade'
-docker-compose run php composer update
+docker-compose run php /bin/sh -c 'composer update && composer sync-recipes --force'
 echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
